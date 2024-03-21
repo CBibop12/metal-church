d25c023413b0494ce809984aef4292417b689960
KetlinUA <pixel.rivne@gmail.com>
2024-03-21 23:41:22 +0200

    Proxt

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/query":"http://metalchurch.nl:5050/query/",
+    }  
+  } ,
   plugins: [react()],
 })
